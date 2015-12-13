957215acb9c128c39835fa979cb8acdc917675f4
JasonBates <jason@jasonbates.com>
2015-12-13 22:03:29 +0000

    adding in some new routes

--- hello.py
+++ hello.py
@@ -6,12 +6,19 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/hello')
 def hello_world():
-    import pdb; pdb.set_trace()
-    i = 3
-    i = i +1
-    visited = i
-    return "Hello everyone and you!" + str(visited)
+    return "Hello everyone and you!"
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    print(username)
+    return "User %s" % username
+
+@app.route('/userno/<int:userno>')
+def show_user_num(userno):
+    return "User %d " % userno
+
 
 if __name__ == '__main__':
     host = os.getenv('IP', '0.0.0.0')
