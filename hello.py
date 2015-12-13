91fa4741037163467b406df280a30d8e99396243
JasonBates <jason@jasonbates.com>
2015-12-13 22:30:02 +0000

    request methods for POST / GET

--- hello.py
+++ hello.py
@@ -2,23 +2,17 @@
 # hello.py
 import os
 
-from flask import Flask
+from flask import Flask, request
 app = Flask(__name__)
 
-@app.route('/')
-@app.route('/hello')
-def hello_world():
-    return "Hello everyone and you!"
-
-@app.route('/user/<username>')
-def show_user_profile(username):
-    print(username)
-    return "User %s" % username
-
-@app.route('/userno/<int:userno>')
-def show_user_num(userno):
-    return "User %d " % userno
-
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        return request.values['username']
+    return '<form method="POST" action="/login"> \
+            <input type="text" name="username" /> \
+            <p><button type="submit">submit</button> \
+            </form>'
 
 if __name__ == '__main__':
     host = os.getenv('IP', '0.0.0.0')
